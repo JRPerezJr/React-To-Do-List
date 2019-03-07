f227ced89d111a984f54957e25245d4bb6302528
Juan P <pront23@gmail.com>
2019-03-07 22:31:20 +0900

    Added all routes to jsonplaceholder. Added all PropTypes. Project built for deployment.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import uuid from 'uuid';
+//import uuid from 'uuid'; was no longer needed
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
 
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 
+
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Pay Rent',
-        completed: false
-      },
-
-      {
-        id: uuid.v4(),
-        title: 'Pay Utilities',
-        completed: false
-      },
-
-      {
-        id: uuid.v4(),
-        title: 'Meet with Kanojo',
-        completed: false
-      }
-    ]
-
+    todos: []
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.setState({ todos: res.data }))
   }
   // Toggle Complete function
   markComplete = (id) => {
@@ -44,18 +31,26 @@ class App extends Component {
   // Delete Todo function
   delTodo = (id) => {
     console.log(id)
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
+    
+    // this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
   }
   // Add Todo
   addTodo = (title) => {
-    console.log(title)
-    const newTodo = {
-      id: uuid.v4(),
-      title,
-      completed: false
-
-    }
-    this.setState({ todos: [...this.state.todos, newTodo] });
+    // console.log(title)
+    // const newTodo = {
+    //   id: uuid.v4(),
+    //   title,
+    //   completed: false No longer needed
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title, 
+        completed: false
+      })
+        .then(res => this.setState({ todos:
+        [...this.state.todos, res.data] }));
+    
+    // this.setState({ todos: [...this.state.todos, newTodo] });
   }
   render() {
     console.log(this.state.todos)  //Lifecycle method needed to render the component
@@ -69,7 +64,7 @@ class App extends Component {
                 <AddTodo addTodo={this.addTodo} />
                   <Todos todos={this.state.todos} markComplete={this.markComplete}
                     delTodo={this.delTodo}
-                  />
+                    />
               </React.Fragment>
             )}/>
             <Route path="/about" component={About}/>
@@ -83,3 +78,21 @@ class App extends Component {
 
 
 export default App;
+
+// {
+//   id: uuid.v4(),
+//   title: 'Pay Rent',
+//   completed: false
+// },
+
+// {
+//   id: uuid.v4(),
+//   title: 'Pay Utilities',
+//   completed: false
+// },
+
+// {
+//   id: uuid.v4(),
+//   title: 'Meet with Kanojo',
+//   completed: false
+// } Removed from Todos array 
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class AddTodo extends Component {
     state = {
@@ -30,6 +31,13 @@ export class AddTodo extends Component {
       </form>
     )
   }
+}
+
+//PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 }
 
 export default AddTodo
