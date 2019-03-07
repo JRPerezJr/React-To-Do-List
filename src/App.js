9f70bfcf198f7cc4676ee77fd17680033547b359
Juan P <pront23@gmail.com>
2019-03-07 16:40:12 +0900

    Added Todos component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Todos from './components/Todos';
+
 class App extends Component {
-  render() {  //Lifecycle method needed to render the component
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+
+      {
+        id: 2,
+        title: 'Dinner with Wife',
+        completed: false
+      },
+
+      {
+        id: 3,
+        title: 'Meeting with friends',
+        completed: false
+      }
+    ]
+
+  }
+  render() {
+    console.log(this.state.todos)  //Lifecycle method needed to render the component
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
